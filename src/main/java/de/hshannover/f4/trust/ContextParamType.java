95a77471cf7542bdde45a32b1d9576712f8885d6
Thomas Rossow <rosso@parttimegeeks.net>
2013-12-18 11:40:30 +0100

    Removes trailing whitespaces from license template

--- src/main/java/de/hshannover/f4/trust/ContextParamType.java
+++ src/main/java/de/hshannover/f4/trust/ContextParamType.java
@@ -1,16 +1,16 @@
 /*
  * #%L
  * =====================================================
- *    _____                _     ____  _   _       _   _
+ * 
  *   |_   _|_ __ _   _ ___| |_  / __ \| | | | ___ | | | |
  *     | | | '__| | | / __| __|/ / _` | |_| |/ __|| |_| |
  *     | | | |  | |_| \__ \ |_| | (_| |  _  |\__ \|  _  |
  *     |_| |_|   \__,_|___/\__|\ \__,_|_| |_||___/|_| |_|
  *                              \____/
- *  
+ * 
  *  =====================================================
  * 
- * Hochschule Hannover 
+ * Hochschule Hannover
  * (University of Applied Sciences and Arts, Hannover)
  * Faculty IV, Dept. of Computer Science
  * Ricklinger Stadtweg 118, 30459 Hannover, Germany
@@ -18,7 +18,7 @@
  * Email: trust@f4-i.fh-hannover.de
  * Website: http://trust.f4.hs-hannover.de/
  * 
- * This file is part of irondetect, version 0.0.5, implemented by the Trust@HsH 
+ * This file is part of irondetect, version 0.0.5, implemented by the Trust@HsH
  * research group at the Hochschule Hannover.
  * %%
  * Copyright (C) 2010 - 2013 Trust@HsH
